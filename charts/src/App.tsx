06ef298c5ada13dab23a8271b6ef7a0d2029dc1a
Victor Prokhorov <mail@victorprokhorov.com>
2022-06-28 22:50:02 +0200

    quick changes

--- charts/src/App.tsx
+++ charts/src/App.tsx
@@ -4,85 +4,182 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { ReactComponent as TrendArrowUp } from './assets/trend-arrow-up.svg';
 import { ReactComponent as TrendArrowDown } from './assets/trend-arrow-down.svg';
-// import Card from './components/Card';
 
-const options = {
-  chart: {
-    type: 'spline',
-  },
-  title: {
-    text: 'My chart',
-  },
-  series: [
-    {
-      data: [1, 2, 1, 4, 3, 6],
-    },
-    {
-      data: [2, 4, 2, 5, 4, 7],
-    },
+const k = {
+
+  }
+
+const dataAverageTicketSize = [
+  59.52,
+  0.17,
+  [
+    ['2021/9', 59.56],
+    ['2021/10', 59.7],
+    ['2021/11', 59.91],
+    ['2021/12', 59.38],
+    ['2021/13', 59.14],
+    ['2021/14', 59.37],
+    ['2021/15', 59.51],
+    ['2021/16', 58.08],
+    ['2021/17', 60.78],
+    ['2021/18', 59.98],
+    ['2021/19', 60.92],
+    ['2021/20', 59.49],
+    ['2021/21', 59.82],
+    ['2021/22', 62.18],
   ],
-};
-const transactionsPerWeekDay: Highcharts.Options = {
+  [
+    ['2022/9', 57.39],
+    ['2022/10', 60.04],
+    ['2022/11', 60.03],
+    ['2022/12', 57.48],
+    ['2022/13', 60.11],
+    ['2022/14', 59.47],
+    ['2022/15', 58.88],
+    ['2022/16', 61.05],
+    ['2022/17', 60.45],
+    ['2022/18', 59.89],
+    ['2022/19', 59.05],
+    ['2022/20', 58.79],
+    ['2022/21', 58.55],
+    ['2022/22', 58.5],
+  ],
+];
+
+const averageTicket: Highcharts.Options = {
   chart: {
-    type: 'column',
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    spacingLeft: 0,
+    spacingRight: 0,
   },
-  exporting: {
-    enabled: false,
-  },
+
   credits: {
     enabled: false,
   },
-  series: [
-    {
-      showInLegend: false,
-      data: [39000, 10000, 25000, 39500, 26000, 50000, 0],
-      type: 'column',
-      name: 'Transactions',
-      color: '#006d44',
-    },
-  ],
+
+  exporting: {
+    enabled: false,
+  },
+
   subtitle: {
     text: '',
   },
+
   title: {
     text: '',
   },
-  xAxis: {
-    categories: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-    labels: {
-      style: {
-        fontSize: '10px',
-      },
 
+  plotOptions: {
+    line: {
+      dataLabels: {
+        enabled: false,
+      },
     },
 
+    series: {
+      marker: {
+        enabled: false,
+      },
+    },
   },
 
   yAxis: {
+    labels: {
+      enabled: false,
+    },
 
     title: {
-
       text: '',
-
     },
 
-    labels: {
+    lineWidth: 0,
 
-      format: '{value}',
+    gridLineWidth: 0,
 
-      style: {
+    tickWidth: 0,
 
-        fontSize: '10px',
+    minPadding: 0,
 
-      },
-
-    },
-
-    tickInterval: 10000,
-
+    maxPadding: 0,
   },
 
+  xAxis: {
+    labels: {
+      enabled: false,
+
+      staggerLines: 0,
+    },
+
+    lineWidth: 0,
+
+    gridLineWidth: 0,
+
+    tickWidth: 0,
+
+    minPadding: 0,
+
+    maxPadding: 0,
+  },
+
+  legend: {
+    enabled: false,
+  },
+
+  series: [
+    {
+      name: 'past',
+
+      type: 'area',
+
+      color: '#006d44',
+
+      opacity: 0.2,
+
+      data: [
+        ['2022/9', 57.39],
+        ['2022/10', 60.04],
+        ['2022/11', 60.03],
+        ['2022/12', 57.48],
+        ['2022/13', 60.11],
+        ['2022/14', 59.47],
+        ['2022/15', 58.88],
+        ['2022/16', 61.05],
+        ['2022/17', 60.45],
+        ['2022/18', 59.89],
+        ['2022/19', 59.05],
+        ['2022/20', 58.79],
+        ['2022/21', 58.55],
+        ['2022/22', 58.5],
+      ],
+    },
+    {
+      name: 'present',
+      color: '#006d44',
+      type: 'line',
+      data: [
+        ['2022/9', 57.39],
+        ['2022/10', 60.04],
+        ['2022/11', 60.03],
+        ['2022/12', 57.48],
+        ['2022/13', 60.11],
+        ['2022/14', 59.47],
+        ['2022/15', 58.88],
+        ['2022/16', 61.05],
+        ['2022/17', 60.45],
+        ['2022/18', 59.89],
+        ['2022/19', 59.05],
+        ['2022/20', 58.79],
+        ['2022/21', 58.55],
+        ['2022/22', 58.5],
+      ],
+      // data: this.getData(this.dataAverageTicketSize[3]),
+    },
+  ],
 };
+
 interface CardContainerProps {
   children?: ReactNode;
 }
@@ -110,7 +207,7 @@ function Card({ title, value, trend, previousValue }: CardProps) {
         <p>vs previous year</p>
       </div>
       <div>
-        <HighchartsReact highcharts={Highcharts} options={options} />
+        <HighchartsReact highcharts={Highcharts} options={averageTicket} />
       </div>
     </div>
   );
