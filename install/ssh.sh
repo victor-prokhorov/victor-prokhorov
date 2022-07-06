481f8dbe3925eaa1e0865045037697b61ea30760
Victor <mail@victorprokhorov.com>
2022-07-06 14:38:44 +0200

    set globals

--- install/ssh.sh
+++ install/ssh.sh
@@ -1,3 +1,5 @@
+git config --global user.name "Victor Prokhorov"
+git config --global user.email "mail@victorprokhorov.com"
 ssh-keygen -t ed25519 -C "mail@victorprokhorov.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
