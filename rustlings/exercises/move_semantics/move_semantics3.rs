0629623feb6a63b60c382141edb0dedfe3db2fde
Victor Prokhorov <mail@victorprokhorov.com>
2022-07-11 18:55:52 +0200

    11/07/2022 18:55:52

--- rustlings/exercises/move_semantics/move_semantics3.rs
+++ rustlings/exercises/move_semantics/move_semantics3.rs
@@ -3,24 +3,42 @@
 // (no lines with multiple semicolons necessary!)
 // Execute `rustlings hint move_semantics3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&mut vec0); // mutable borrow
+                                        // vec1 had the borrow on vec0
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
     vec1.push(88);
+    // this mean we cannot mutate already borrowed
+    // vec0.push(123);
+    // you cannot have any other borrow not even non-mutable
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    // this was the last line vec1 was used
+    // vec0 can be borrowed now
+    // lets try to do multiple non mutable borrow
+    let v0 = check_if_i_got_this(&vec0);
+    let v1 = check_if_i_got_this(&v0);
+    println!("{:?}, {:?}", v0, v1);
+
+    // v0 and v1 droped
+    // we can mutate again
+    vec0.push(1234);
+    println!("{:?}", vec0);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &mut Vec<i32>) -> &mut Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec // we return mutable borrow, we still hold it
+}
+
+fn check_if_i_got_this(vec: &Vec<i32>) -> &Vec<i32> {
+    println!("{:?}", vec);
+    return vec;
 }
--- rustlings/exercises/move_semantics/move_semantics6.rs
+++ rustlings/exercises/move_semantics/move_semantics6.rs
@@ -2,24 +2,37 @@
 // Make me compile! `rustlings hint move_semantics6` for hints
 // You can't change anything except adding or removing references
 
-// I AM NOT DONE
-
+// https://doc.rust-lang.org/rust-by-example/scope/move.html
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
+
+    let mut mut_vec: Vec<i32> = vec![1, 2, 3];
+    let mut mut_vec_moved = try_it(mut_vec); // moved here because not impl 'Copy' trait
+
+    // cannot borrow from moved value
+    // println!("{:?}", mut_vec);
+
+    println!("{:?}", mut_vec_moved); // but can borrow because moved to mut_vec_moved
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{}", data);
+}
+
+fn try_it(mut vec: Vec<i32>) -> Vec<i32> {
+    vec[0] = 666;
+    println!("{:?}", vec);
+    vec
 }
