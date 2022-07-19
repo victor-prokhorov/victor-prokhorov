4a1abcfdaefdde051763298409217fa3bccb95d1
Victor Prokhorov <mail@victorprokhorov.com>
2022-07-19 09:34:55 +0200

    tokio serial and concurent quick check

--- tokio-async-fund1/src/main.rs
+++ tokio-async-fund1/src/main.rs
@@ -1,5 +1,5 @@
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
-
 use tokio::time::sleep;
 
 // TODO:
@@ -9,45 +9,82 @@ use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // 5, 3, 1 at 5+3+1
-    delay5().await;
-    delay3().await;
-    delay1().await;
+    let response = all().await?;
+    assert_eq!(response[0], 1);
+    assert_eq!(response[1], 3);
+    assert_eq!(response[2], 5);
+    Ok(())
+}
+
+enum TypeOfAsync {
+    Concurrent,
+    Serial,
+}
+
+async fn all() -> Result<Vec<i32>, Box<dyn std::error::Error>> {
+    use TypeOfAsync::*;
+    let result: Vec<i32> = vec![];
+
+    // 5, 3, 1
+    // cargo test
+    // finished in 9.01s
+    // delay(5, Serial).await;
+    // delay(3, Serial).await;
+    // delay(1, Serial).await;
+    // return Ok(result);
+
+    let arc = Arc::new(Mutex::new(result));
 
     // TODO:
     // concurrently run all now
-    // 1, 3, 5 at 5
+    // 1, 3, 5
+    // cargo test
+    // finished in 5.00s
     let mut handles = vec![];
     for i in 0..3 {
+        let a = arc.clone();
         // called `task`
-        // same syntax as `tread`
+        // same syntax as `thread`
         // we can use Arc and Mutex almost the same way
         handles.push(tokio::spawn(async move {
-            match i {
-                0 => delay5().await,
-                1 => delay3().await,
-                2 => delay1().await,
+            let n = match i {
+                0 => delay(5, Concurrent).await,
+                1 => delay(3, Concurrent).await,
+                2 => delay(1, Concurrent).await,
                 _ => panic!(),
-            }
+            };
+            let mut guard = a.lock().unwrap();
+            let v = &mut *guard;
+            v.push(n);
         }));
     }
     for handle in handles {
         handle.await.unwrap();
     }
-
-    println!("main");
-    Ok(())
+    let guard = arc.lock().unwrap();
+    // deref to get value and not mutex
+    // borrow and not move then clone
+    let v = &*guard;
+    Ok(v.to_vec())
+}
+async fn delay(delay: u64, label: TypeOfAsync) -> i32 {
+    sleep(Duration::from_secs(delay)).await;
+    match label {
+        TypeOfAsync::Serial => println!("serial{}", delay),
+        TypeOfAsync::Concurrent => println!("concurrent{}", delay),
+    }
+    delay.try_into().unwrap()
 }
 
-async fn delay5() {
-    sleep(Duration::from_secs(5)).await;
-    println!("awaited 5");
-}
-async fn delay3() {
-    sleep(Duration::from_secs(3)).await;
-    println!("awaited 3");
-}
-async fn delay1() {
-    sleep(Duration::from_secs(1)).await;
-    println!("awaited 1");
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn tasks_was_run_concurently() {
+        let response = all().await.unwrap();
+        assert_eq!(response[0], 1);
+        assert_eq!(response[1], 3);
+        assert_eq!(response[2], 5);
+    }
 }
