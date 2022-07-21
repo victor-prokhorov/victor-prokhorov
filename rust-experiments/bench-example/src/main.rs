47f318b2703de44c7f43593817ff3e49b0d4ef48
Victor Prokhorov <mail@victorprokhorov.com>
2022-07-21 11:14:22 +0200

    21/07/2022 11:14:22

---
+++ rust-experiments/bench-example/src/main.rs
@@ -0,0 +1,33 @@
+#![feature(test)]
+
+fn main() {}
+
+extern crate test;
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[bench]
+    fn bench_add_two(b: &mut Bencher) {
+        b.iter(|| add_two(2));
+    }
+
+    #[bench]
+    fn bench_another(b: &mut Bencher) {
+        b.iter(|| {
+            println!("another bench");
+            0
+        });
+    }
+}
